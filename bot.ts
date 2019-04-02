b4f5a408881eb831b546b65157c0751230c37e0c
Adrian Sampson <adrian@radbox.org>
2019-04-02 19:50:45 -0400

    Proof of concept

--- bot.ts
+++ bot.ts
@@ -3,9 +3,24 @@ import * as http from 'http';
 
 const PORT = 8323;
 
+function send(res: http.OutgoingMessage, data: any) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(data));
+}
+
 let routes = [
   new libweb.Route('/lock', async (req, res, params) => {
-    console.log("lock");
+    let data = await libweb.formdata(req);
+    console.log(`${data.user_name} (${data.user_id}) acquired ` +
+      `${data.text} in ${data.channel_id}`);
+    send(res, {
+      response_type: 'in_channel',
+      text: `${data.user_name} :lock2: ${data.text}`,
+    });
+  }),
+  new libweb.Route('/unlock', async (req, res, params) => {
+    console.log("unlock");
+    res.end(JSON.stringify({'foo': 'hi'}));
   }),
 ];
 
